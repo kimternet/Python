c1f1ca86e1d315a1c6e976c83bda0f0e0224f2a4
Hyungwoon_Kim <126911740+kimternet@users.noreply.github.com>
2024-03-16 20:41:07 +0900

    Chap2.Variable
    #변수를 선언하는 다양한 방법들 학습

---
+++ _02_Variable.py
@@ -0,0 +1,53 @@
+# -- Str , Int --
+
+# age = 21
+# #age += 1
+# print(type(age))  # int
+
+
+# #age = "21"
+# print(type(age))  # str
+# print("Your age is : " + str(age))
+
+
+# --float, boolean--
+
+# height = 250.5
+# print(height)
+# print(type(height))  # float
+
+
+#print("your height is : " + str(height))
+
+# ==== Next ======
+
+#human = False
+#print(human)
+
+#print(type(human))  # bool
+
+# multiple assignment 다중할당
+
+# --기본 방법--
+
+#name = "Bro"
+#age = 21
+#attractive = True
+
+#print(name)
+#print(age)
+#print(attractive)
+
+# -- 방법 1 --
+
+# name, age, attractive = "Bro", 21, True
+# print(name,age,attractive)
+
+
+# -- 방법 2 ---
+
+
+#me = you = friends =30
+#print(me, you, friends)
+
+
