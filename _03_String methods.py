8f07912e8934f9dcb8ed967de3f56cf9cc0aa329
Hyungwoon_Kim <126911740+kimternet@users.noreply.github.com>
2024-03-17 22:47:38 +0900

    _03_StringMethods
    String의 다양한 변형법을 학습

---
+++ _03_String methods.py
@@ -0,0 +1,15 @@
+name ="kimhyeongwoon"
+name2 ="6"
+
+#print(len(name))
+print(name.find("k"))
+print(name)
+print(name.capitalize()) # 맨 앞글자만 대문자로 변환
+print(name.upper())
+print(name.lower())
+print(name2.isdigit())  #문자열이 숫자인지 아닌지
+print(name2.isalpha())  #알파벳 문자인지
+print(name.count("o"))  # o가 몇개인지
+print(name.replace("o","O"))  # o를 -> O로 바꾸고 싶을때
+print(name * 3)
+ 
