7d18b6995d87f33d71f0c3336b5bf6ed6754f43a
Hyungwoon_Kim <126911740+kimternet@users.noreply.github.com>
2024-03-18 23:38:43 +0900

    2.chap
    변수와 함수 그리고 return의 개념을 배운 뒤 계산기와 주스 제조기를 만들면서 개념 학습

---
+++ 2.chap/_01_Parameters.py
@@ -0,0 +1,16 @@
+def say_hello(user_name):
+    print("hello", user_name,"how are you ? ")
+    
+
+say_hello("kim")  # 데이터 값을 function에 넣고 function은 데이터를 받아 사용
+
+
+def say_hello(user_name, user_age):
+    print("Hallo", user_name)
+    print("you are", user_age,"years old")
+    
+    
+say_hello("kim", 12)
+
+
+
---
+++ 2.chap/_02_Recap.py
@@ -0,0 +1,9 @@
+def tax_calculator(money):
+    print(money * 0.35)
+    
+
+tax_calculator(150000)
+tax_calculator(1500)
+
+
+    
