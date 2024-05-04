a92f767ec74ed9e9a889d13e7162678253cfcc94
Hyungwoon_Kim <kimternet23@gmail.com>
2024-05-05 00:20:53 +0900

    Delete 2.chap directory

--- 2.chap/_02_Recap.py
+++
@@ -1,9 +0,0 @@
-def tax_calculator(money):
-    print(money * 0.35)
-    
-
-tax_calculator(150000)
-tax_calculator(1500)
-
-
-    
--- 2.chap/_03_DefaltParameter.py
+++
@@ -1,6 +0,0 @@
-def say_hello(user_name="anonymous"):  # username을 기본값으로 설정
-    print("hello", user_name)
-    
-
-say_hello("kim")  # kim 출력
-say_hello() # default값 출력
